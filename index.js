dd600e57c7e867ae8c411aab338d88b204ac8347
Raghu02496 <lawlieta023@gmail.com>
2026-01-07 12:20:28 +0530

    feat: json and cors middleware added

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 import 'dotenv/config';
 import express from 'express'
 import { aiRouter } from './routes.js';
+import cors from "cors"
 
-const app = express()
+const app = express();
+app.use(
+  express.json(),
+  cors({
+    origin : 'http://localhost:4200'
+  })
+)
 app.use('/ai', aiRouter)
 
 app.listen(5000, ()=>{
