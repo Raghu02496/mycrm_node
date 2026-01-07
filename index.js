56960d5b8eb345c83d2719fcfff1d3f05cd92f58
Raghu02496 <lawlieta023@gmail.com>
2026-01-07 11:38:54 +0530

    feat : test interaction added with gemini

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Hello node')
+import 'dotenv/config';
+import { googleAI } from '@genkit-ai/google-genai';
+import { genkit } from 'genkit';
+
+// Initialize Genkit with the Google AI plugin
+const ai = genkit({
+  plugins: [googleAI()],
+  model: googleAI.model('gemini-2.5-flash-lite', {
+    temperature: 0.8,
+    apiKey : process.env.GEMINI_API_KEY,
+  }),
+});
+
+const res = await ai.generate("Hello how are you, do u know my name")
+console.log(res.text)
